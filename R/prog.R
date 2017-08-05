5052d79731f825fc2c261da08cc7f8eb95414dc2
tsdye <tsd@tsdye.com>
2017-08-04 17:09:24 -1000

    OccurrencePlot, add new function

--- R/prog.R
+++ R/prog.R
@@ -1747,3 +1747,120 @@ TempoActivityPlot <- function(data, position, level = 0.95, count = TRUE, title 
   plot(x<-t[-1], y<-diff(moy)/diff(t), type = "l", xlab="Time", ylab="Incidence", main =title)
   grid()
 }
+
+###################################
+###       Occurrence plot       ###
+###################################
+
+##library(ggplot2)
+##library(ggalt)
+##library(toOrdinal)
+
+#' @param data dataframe containing the output of the MCMC algorithm
+#' @param position numeric vector containing the position of the column corresponding to the MCMC chains of interest
+#' @param level probability corresponding to the level of confidence
+#' @param intervals one of "CI" for credible intervals or "HPD" for highest posterior density intervals
+#' @param title title of the graph
+#' @param subtitle subtitle of the graph
+#' @param caption caption of the graph
+#' @param labelXaxis label of the x-axis
+#' @param labelYaxis label of the y-axis
+#' @param language a string indicating a language recognized by the toOrdinal library
+#' @param occurrence a string to append to each y axis tic label
+#' @param width width size in units
+#' @param height height size in units
+#' @param units a string recognized by the ggsave function, one of "in", "cm", "mm"
+#' @param x.min minimum x axis value
+#' @param x.max maximum x axis value
+#' @param x.scale one of "calendar" for calendar years, "bp" for years before present, or "elapsed" for time elapsed from a specified origin
+#' @param elapsed.origin.position position of the column to use as the origin for elapsed time calculations
+#' @param dumbbell.size size of the plot symbol
+#' @param dot.guide switch for a horizontal guide from the y axis
+#' @param dot.guide.size size of the dot guide
+#' @param y.grid switch for horizontal grid lines
+#' @param count, if TRUE the counting process is a number, otherwise it is a probability
+#' @param file the name of the graph (+ extension) that will be saved if chosen. Null by default.
+#' @return a plot
+#' @export
+
+OccurrencePlot <- function(data, position, level = 0.95, intervals = "CI",
+                           title = "Occurrence plot",
+                           subtitle = NULL,
+                           caption = "ArchaeoPhases",
+                           labelXaxis = "Calendar year",
+                           labelYaxis = NULL,
+                           language = "English", occurrence = "event",
+                           height = 7, width = 7, units = "in",
+                           x.min = NULL, x.max = NULL, x.scale = "calendar",
+                           elapsed.origin.position = NULL,
+                           dumbbell.size = 3, dot.guide = FALSE,
+                           dot.guide.size = 0.25, y.grid = FALSE,
+                           count = TRUE, file = NULL)
+{
+  library(ggplot2)
+  library(ggalt)
+  library(toOrdinal)
+  sort.rows <- function(x) {
+    if (is.numeric(as.matrix(x))) {
+      res <- as.data.frame(t(apply(x,1,sort)))
+      colnames(res) <- 1:ncol(res)
+      res
+    }
+    else {
+      stop("Cannot sort non-numeric data frame")
+    }
+  }
+  group.dates <- function(data, position) {
+    L = length(position)
+    res = matrix(ncol = L, nrow=nrow(data))
+    for (i in 1:L) {
+      res[,i] = data[,position[i]]
+    }
+   res
+  }
+  if (x.scale == "elapsed") {
+    if (is.null(elapsed.origin.position)) {
+      stop("Elapsed origin not specified")
+    }
+    else {
+      data <- data - data[,elapsed.origin.position]
+    }
+  }
+groupOfDates <- group.dates(data, position)
+groupOfEvents <- sort.rows(groupOfDates)
+if (intervals == "CI") {
+  Bornes = MultiCredibleInterval(groupOfEvents, 1:ncol(groupOfEvents),
+                                 level = level)
+}
+else if (intervals == "HPD") {
+  Bornes = MultiHPD(groupOfEvents, 1:ncol(groupOfEvents), level = level)
+}
+  Bornes.df <- as.data.frame(Bornes)
+  if (x.scale == "bp") {
+    Bornes.df$CredibleIntervalInf <- 1950-Bornes.df$CredibleIntervalInf
+    Bornes.df$CredibleIntervalSup <- 1950-Bornes.df$CredibleIntervalSup
+  }
+  Bornes.df$y.labs <- factor(as.integer(row.names(Bornes.df)),
+                           labels = paste(sapply(as.numeric(row.names(Bornes.df)),
+                                                 toOrdinal, language),
+                                          occurrence, sep= " "))
+  h <- ggplot(data = Bornes.df, aes(y=y.labs, x=Bornes.df[,2],
+                                    xend=Bornes.df[,3])) +
+    geom_dumbbell(size = dumbbell.size, dot_guide = dot.guide,
+                  dot_guide_size = dot.guide.size) +
+    labs(x = labelXaxis, y = labelYaxis, title = title,
+         subtitle = subtitle, caption = caption)
+  if (!y.grid) {
+    h <- h + theme(panel.grid.major.y=element_blank())
+  }
+  if (!is.null(x.min) & !is.null(x.max)) {
+    h <- h + xlim(x.min, x.max)
+  }
+  if (!is.null(file)) {
+    ggsave(filename = file, plot = h, height = height,
+           width = width, units = units)
+  }
+  dev.new(height = height, width = width)
+  print(h)
+  list(Bornes.df)
+}
